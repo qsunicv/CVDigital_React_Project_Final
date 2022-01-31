7b3802d01f0dadb744a8e9beeffd8213832f6851
qsunicv <66311000+qsunicv@users.noreply.github.com>
2022-01-31 15:59:32 -0100

    update4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { Footer, Blog, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
 
 import './App.css';
@@ -14,7 +14,6 @@ const App = () => (
     <Brand />
     <WhatGPT3 />
     <Features />
-    <Possibility />
     <CTA />
     <Blog />
     <Footer />
